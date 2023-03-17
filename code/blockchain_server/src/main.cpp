92e9f0e9e5664bd1efcf7d9c4d60bf093b487dfb
jbruel <jonathan.bruel@epitech.eu>
2023-03-17 09:39:44 +0100

    Add mining_main in seperate thread

--- code/blockchain_server/src/main.cpp
+++ code/blockchain_server/src/main.cpp
@@ -1,36 +1,24 @@
 #include "server/listener.hpp"
 
+#include <centor/blockchain.hpp>
 #include <spdlog/spdlog.h>
+#include <thread>
+
+void mining_main()
+{
+  centor::blockchain blockchain;
+
+  spdlog::info("Mining while is starting.");
+  for (int block_number = 1; true; ++block_number)
+    blockchain.add_block(block_number, fmt::format("Block number {}.", block_number));
+}
 
 int main()
 {
   srv::listener http_server_listener("localhost", 4242);
+  std::jthread mining_thread(&mining_main);
 
   spdlog::info("Running HTTP server");
   http_server_listener.listen();
   return EXIT_SUCCESS;
 }
-
-//#include <centor/blockchain.hpp>
-//
-//#include <chrono>
-//#include <fmt/format.h>
-//#include <future>
-//#include <spdlog/spdlog.h>
-//
-//int main()
-//{
-//  centor::blockchain blockchain;
-//  int block_number = 1;
-//
-//  spdlog::info("Custom blockchain is launched.");
-//  while (true)
-//  {
-//    auto add_block = std::async(std::launch::async,
-//                                [&blockchain, &block_number]()
-//                                { blockchain.add_block(block_number, fmt::format("Block number {}.", block_number)); });
-//    add_block.wait();
-//    ++block_number;
-//  }
-//  return EXIT_SUCCESS;
-//}
